4da7b80347f2607f036d0c18e729947b0d2c627a
notzree <notzree@gmail.com>
2024-03-13 11:34:13 -0400

    Mock implementation

--- pathfinder.go
+++ pathfinder.go
@@ -1,13 +1,21 @@
 package main
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 type PathFinder interface {
 	FindPath(ctx context.Context, from, to string) ([]string, error)
 }
 
-type WikigraphPathFinder struct{ graph_path string }
+type WikigraphPathFinder struct {
+	graph_path string
+	db         *sql.DB
+}
 
 func (w *WikigraphPathFinder) FindPath(ctx context.Context, from, to string) ([]string, error) {
+	print("Finding path from ", from, " to ", to)
+	return []string{"path1", "path2"}, nil
 
 }
