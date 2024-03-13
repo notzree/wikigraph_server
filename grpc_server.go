6989c51ef6577c2912d01523afa7d61bd2b0ed86
notzree <notzree@gmail.com>
2024-03-13 10:47:30 -0400

    Setup gRPC + protobuf

---
+++ grpc_server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/notzree/wikigraph_server/proto"
+	"google.golang.org/grpc"
+)
+
+func BuildAndRunGRPCServer(svc PathFinder, listenAddr string) error {
+	grpcPathFinder := NewGRPCPathFinderServer(svc)
+
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+	opts := []grpc.ServerOption{}
+	server := grpc.NewServer(opts...)
+	proto.RegisterPathFinderServer(server, grpcPathFinder)
+	return server.Serve(ln)
+
+}
+
+type GRPCPathFinderServer struct {
+	svc PathFinder
+	proto.UnimplementedPathFinderServer
+}
+
+func NewGRPCPathFinderServer(svc PathFinder) *GRPCPathFinderServer {
+	return &GRPCPathFinderServer{
+		svc: svc,
+	}
+}
+
+func (pf *GRPCPathFinderServer) FindPath(ctx context.Context, req *proto.PathRequest) (*proto.PathResponse, error) {
+	paths, err := pf.svc.FindPath(ctx, req.From, req.To) // this way the server does not need to know any business logic!
+	if err != nil {
+		return nil, err
+	}
+	resp := &proto.PathResponse{Paths: paths}
+
+	return resp, err
+}
