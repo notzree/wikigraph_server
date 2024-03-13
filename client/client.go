eee38c2cd004ebec6b2d998da58f047bdc683de8
notzree <notzree@gmail.com>
2024-03-13 11:33:38 -0400

    create gRPC client

---
+++ client/client.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"github.com/notzree/wikigraph_server/proto"
+	"google.golang.org/grpc"
+)
+
+func NewGRPCClient(remoteAddr string) (proto.PathFinderClient, error) {
+	conn, err := grpc.Dial(remoteAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	c := proto.NewPathFinderClient(conn)
+
+	return c, nil
+}
