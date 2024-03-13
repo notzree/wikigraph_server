9cc5710b2c69699d1c8d3a7e24a385f340571604
notzree <notzree@gmail.com>
2024-03-13 10:48:24 -0400

    idiomatic variable declarations

--- main.go
+++ main.go
@@ -6,25 +6,34 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
 
 const MAX_REQ_PER_HOUR = 10
 
 func main() {
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	var ctx = context.Background()
-	opt, _ := redis.ParseURL(os.Getenv("UPSTASH_URL"))
-	client := redis.NewClient(opt)
-	exampleService := &ExampleService{}
-	rate_limited_service := NewRateLimitedService(exampleService, client, ctx, 60, MAX_REQ_PER_HOUR)
-	server := NewApiServer(":8080")
-	server.RegisterService("/example", rate_limited_service)
+	var (
+		ctx     = context.Background()
+		opt, _  = redis.ParseURL(os.Getenv("UPSTASH_URL"))
+		client  = redis.NewClient(opt)
+		rl_port = os.Getenv("RATE_LIMITER_PORT")
+		pf_port = os.Getenv("PATH_FINDER_PORT")
+	)
 
-	if err := server.Start(); err != nil {
+	//start pathfinder grpc service (grpc)
+	go BuildAndRunGRPCServer(&WikigraphPathFinder{}, pf_port)
+
+	//start rate_limiter service (json)
+	rate_limited_service := NewRateLimitedService(client, ctx, 60, MAX_REQ_PER_HOUR)
+	rate_limit_server := NewApiServer(rl_port)
+	rate_limit_server.RegisterService("/find_path", rate_limited_service)
+	if err := rate_limit_server.Start(); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
 }
