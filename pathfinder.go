d69a20e9b578db82d87121d7300dda5d127c0cf8
notzree <notzree@gmail.com>
2024-03-17 15:10:37 -0400

    Correct lookup query

--- pathfinder.go
+++ pathfinder.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"log"
 
 	g "github.com/notzree/wikigraph_server/graph"
 )
@@ -46,6 +47,7 @@ func (w *WikigraphPathFinder) LookupByOffset(offset int32) (string, error) {
 	var title string
 	err := w.db.QueryRow("SELECT title FROM lookup WHERE byteoffset = $1", offset).Scan(&title)
 	if err != nil {
+		log.Println("lookupbyoffset error for :", offset, "err:", err)
 		return "", err
 	}
 	return title, nil
@@ -53,9 +55,11 @@ func (w *WikigraphPathFinder) LookupByOffset(offset int32) (string, error) {
 
 func (w *WikigraphPathFinder) LookupByTitle(title string) (int32, error) {
 	var byteoffset int
-	err := w.db.QueryRow("SELECT byteoffset FROM lookup WHERE title = $1", title).Scan(&byteoffset)
+	err := w.db.QueryRow("(SELECT lookup.byteoffset FROM lookup WHERE title = $1 LIMIT 1)UNION ALL (SELECT lookup.byteoffset FROM redirect INNER JOIN lookup ON redirect.redirect_to = lookup.title WHERE redirect.redirect_from = $1 LIMIT 1)LIMIT 1;", title).Scan(&byteoffset)
 	if err != nil {
+		log.Println("LookupByTitle error for :", title, "err:", err)
 		return -1, err
 	}
+	log.Println("LookupByTitle", title, "byteoffset", byteoffset)
 	return int32(byteoffset), nil
 }
